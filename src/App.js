30c565f82d8ef4a16388c564df4421e6e22e20d4
Akul Maheshwari <am2737@cornell.edu>
2025-04-19 17:40:25 -0400

    Added logo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<><Title>Home</Title><Home /></>} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
       </Routes>
